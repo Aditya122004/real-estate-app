1b20705e5acf21d250a8beefdbc0f31cacff68b5
BlunderBoy12 <sharmaaditya122004@gmail.com>
2025-02-06 22:42:51 +0530

    fix:routing issue in user

--- app/(routes)/user/page.jsx
+++ app/(routes)/user/page.jsx
@@ -8,7 +8,7 @@ export default function User() {
   return (
     <div className="my-6 md:px-10 lg:px-32 w-full">
       <h2 className="font-bold text-xl py-3">Profile</h2>
-      <UserProfile>
+      <UserProfile routing="hash">
       <UserProfile.Page
           label='My Listing'
           labelIcon={<Building2 className='h-5 w-5'/>}
@@ -19,5 +19,5 @@ export default function User() {
 
       </UserProfile>
     </div>
-  );
+  ); 
 }
